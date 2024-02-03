fda2de2fc8a0b50ab1714f34098498e1ccbb1bb1
rresnik2 <122578014+rresnik2@users.noreply.github.com>
2024-02-03 13:18:35 -0800

    Update table.js

--- m05/table.js
+++ m05/table.js
@@ -1,11 +1,12 @@
 var table = prompt('Enter a number between 0 - 10: ');             // Unit of table
 var i = 1;                 // Set counter to 1
-var msg = '';              // Message
-
+var msg = '<h3> Multiplication Table </h3>';
+// Do multiplication
 while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
-    i++; 
-}
+  msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+  i++;
+  }
+
 
 // Write the message into the page
 var el = document.getElementById('blackboard');
