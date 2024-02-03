09b593def66492e1b2429a1a106ea0aa9a69027d
rresnik2 <122578014+rresnik2@users.noreply.github.com>
2024-02-03 10:08:59 -0800

    Create table.js

---
+++ m05/table.js
@@ -0,0 +1,21 @@
+var table = prompt('Enter a number between 0 - 10: ');             // Unit of table
+var i = 1;                 // Set counter to 1
+var msg = '';              // Message
+
+if (operator === 'addition') {
+  // Do addition
+  while (i < 11) {
+    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+    i++;
+  }
+} else {
+  // Do multiplication
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+  }
+}
+
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
