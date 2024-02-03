21f1a1945c3fb38e70a66e15d36ee6402af1eebb
rresnik2 <122578014+rresnik2@users.noreply.github.com>
2024-02-03 11:25:00 -0800

    Update table.js

--- m05/table.js
+++ m05/table.js
@@ -2,15 +2,7 @@ var table = prompt('Enter a number between 0 - 10: ');             // Unit of ta
 var i = 1;                 // Set counter to 1
 var msg = '';              // Message
 
-if (operator === 'addition') {
-  // Do addition
-  while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} else {
-  // Do multiplication
-  while (i < 11) {
+while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
