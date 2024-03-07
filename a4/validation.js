9b43ae9cd1e6a3c11c6abb39a547e61034097e21
rresnik2 <122578014+rresnik2@users.noreply.github.com>
2024-03-07 11:27:03 -0800

    Update validation.js

--- a4/validation.js
+++ a4/validation.js
@@ -1,56 +1,78 @@
-function ValitdateForm() {
+function validateForm() {
   var validUsername = false;
   var validUserPassword = false;
   var validFirstName = false;
   var validPhone = false;
   var validEmail = false;
+  var validLastName=false;
+  var userEmail = "";
+  var firstName = "";
+  var LastName = "";
+  var phone = "";
+  var password = "";
+  var userName = "";
 
-  var letters = /^[A-za-z]+$/;
-  var numbers = /^[0-9]+$/;
-  var userEmail = document.getElementById("email").value;
   var atpos = userEmail.indexOf("@");
   var dotpos = userEmail.lastIndexOf(".");
-
+  var letters = /^[A-za-z]+$/;
+  var numbers = /^[0-9]+$/;
   var errorMessages = "";
+  try{
+  userEmail = document.getElementById("Email").value;
+  firstName = document.getElementById("FirstName").value;
+  LastName = document.getElementById("LastName").value;
+  phone = document.getElementById("Phone").value;
+  password = document.getElementById("Password").value;
+  userName = document.getElementById("UserName").value;
+  }
+  catch(Exception)
+    {errorMessages += "";}
 
-  if (myContact.userName.value.length > 12 ||
-    myContact.userName.value.length === null ||
-    myContact.userName)
+  if (userName.length > 12 ||
+    userName === "")
 
     errorMessages += "<p>The username must be less than 12 characters</p>";
   else
     validUsername = true;
 
-  if (myContact.password.value == null ||
-    myContact.password.value.length > 15 ||
-    myContact.password.value == "")
+  if (password.length > 15 ||
+    password === "")
     errorMessages += "<p>The password must be less than 15 characters and it is required</p>";
 
   else
     validUserPassword = true;
-  document.getElementById("errorMessages").innerHTML = errorMessages;
+  
 
-  if (myContact.firstname.value == null || myContact.firstname.value === "" ||
-    myContact.firstname.value.length > 15)
+  if (firstName === "" || firstName.length > 15)
 
-    errorMessages += "<p>Invaled first name.</p>";
+    errorMessages += "<p>The first name must be under 20 chracters and it is required. Only letters and number are accepted.</p>";
 
   else
     validFirstName = true;
 
-  if (myContact.phone.value === "" ||
-    myContact.phone.value == null || myContact.phone.value.length > 15 ||
-    !myContact.phone.value.match(letters))
-    errorMessages += "<p>The phone number is invalid.</p>";
+  if (phone === "" ||
+    phone.length > 15 ||
+    !phone.match(letters))
+    errorMessages += "<p>The phone number mus tbe less than 15 chracters and it is required. Only numbers are accepted.</p>";
 
   else
     validPhone = true;
-
-  if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= userEmail.length)
-    errorMessages += "<p>The email is invalid.</p>";
+  
+  if (isNaN(phone) || phone.length >15 || phone==="")
+    errorMessages += "<p>Invalid phone number </p>";
   else
-    validEmail = true;
+    validPhone = true;
+
+
 
   
-  
+  if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= userEmail.length) {
+    errorMessages += "<p>Invalid email</p>";
+  } else {
+    validEmail = true;
+  } //Or assign the value to a variable. For example validEmail = true
+  if (!validEmail || !validUserPassword || !validUsername || !validFirstName || !validPhone ){
+    document.getElementById("errorMessages").innerHTML = errorMessages;
+    return false;
+  }
 }
